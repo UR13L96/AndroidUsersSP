65cc0222cb0cf92545f24b6a825d1460b9bdf9c4
Uriel <uriel1396@gmail.com>
2023-10-03 09:54:18 -0600

    Added UserAdapter for recycler view

---
+++ app/src/main/java/com/olascoaga/userssp/UserAdapter.kt
@@ -0,0 +1,35 @@
+package com.olascoaga.userssp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.olascoaga.userssp.databinding.ItemUserBinding
+
+class UserAdapter(private val users: List<User>): RecyclerView.Adapter<UserAdapter.ViewHolder>() {
+    private lateinit var context: Context
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
+
+        val view = LayoutInflater.from(context).inflate(R.layout.item_user, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val user = users[position]
+
+        with(holder) {
+            binding.tvIndex.text = "${user.id}"
+            binding.tvName.text = user.name
+        }
+    }
+
+    override fun getItemCount(): Int = users.size
+
+    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val binding = ItemUserBinding.bind(view)
+    }
+}
