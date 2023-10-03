93df2692b3cdd70dd0525259063e453b6065b18c
Uriel <uriel1396@gmail.com>
2023-10-03 10:09:06 -0600

    Add recycler view functionality into activiy

--- app/src/main/java/com/olascoaga/userssp/MainActivity.kt
+++ app/src/main/java/com/olascoaga/userssp/MainActivity.kt
@@ -2,10 +2,37 @@ package com.olascoaga.userssp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.olascoaga.userssp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var userAdapter: UserAdapter
+    private lateinit var linearLayoutManager: RecyclerView.LayoutManager
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        userAdapter = UserAdapter(getUsers())
+        linearLayoutManager = LinearLayoutManager(this)
+
+        binding.recyclerView.apply {
+            layoutManager = linearLayoutManager
+            adapter = userAdapter
+        }
+    }
+
+    private fun getUsers(): MutableList<User> {
+        val users = mutableListOf<User>()
+
+        val alejandro = User(1, "Alejandro", "Abed", "")
+        val yarael = User(2, "Yarael", "Garcia", "")
+
+        users.add(alejandro)
+        users.add(yarael)
+
+        return users
     }
 }
