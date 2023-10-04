ed5a8f41c8923580381e19b47a62e1dc745414b0
Uriel <uriel1396@gmail.com>
2023-10-04 10:32:11 -0600

    Information added as URL and added more data for scrolling

--- app/src/main/java/com/olascoaga/userssp/MainActivity.kt
+++ app/src/main/java/com/olascoaga/userssp/MainActivity.kt
@@ -27,11 +27,27 @@ class MainActivity : AppCompatActivity() {
     private fun getUsers(): MutableList<User> {
         val users = mutableListOf<User>()
 
-        val alejandro = User(1, "Alejandro", "Abed", "")
-        val yarael = User(2, "Yarael", "Garcia", "")
+        val alejandro = User(1, "Alejandro", "Abed", "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&w=1000&q=80")
+        val yarael = User(2, "Yarael", "Garcia", "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&w=1000&q=80")
+        val miles = User(3, "Miles", "Morales", "https://www.infobae.com/new-resizer/b1YhCRwrSa31Zxo4NzLHLH9AIeg=/1200x900/filters:format(webp):quality(85)/cloudfront-us-east-1.images.arcpublishing.com/infobae/4XZXEYWUGNHWBDSIUQBXFYGGFY.jpeg")
+        val frida = User(4, "Frida", "Kahlo", "https://upload.wikimedia.org/wikipedia/commons/0/06/Frida_Kahlo%2C_by_Guillermo_Kahlo.jpg")
 
         users.add(alejandro)
         users.add(yarael)
+        users.add(miles)
+        users.add(frida)
+        users.add(alejandro)
+        users.add(yarael)
+        users.add(miles)
+        users.add(frida)
+        users.add(alejandro)
+        users.add(yarael)
+        users.add(miles)
+        users.add(frida)
+        users.add(alejandro)
+        users.add(yarael)
+        users.add(miles)
+        users.add(frida)
 
         return users
     }
